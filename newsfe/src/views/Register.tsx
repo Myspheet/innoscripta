dfe61827b8be8ef50dde103340c70fac58f3b28d
Promise Oghenevwefe <myspheet@gmail.com>
2025-01-02 12:46:01 +0100

    Update register and login components

--- newsfe/src/views/Register.tsx
+++ newsfe/src/views/Register.tsx
@@ -64,11 +64,6 @@ const Register = () => {
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
